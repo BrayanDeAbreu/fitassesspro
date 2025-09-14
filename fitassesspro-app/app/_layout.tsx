dfdd8f7d3ebb333ca2b911dc1d5cb810a93c525f
brayan <brayandeabreu@gmail.com>
2025-09-14 11:30:40 +0100

    added header in login page

--- fitassesspro-app/app/_layout.tsx
+++ fitassesspro-app/app/_layout.tsx
@@ -1,5 +1,21 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "FitAssessPro",
+          headerStyle: {
+            backgroundColor: '#000000',
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      />
+    </Stack>
+  );
 }
